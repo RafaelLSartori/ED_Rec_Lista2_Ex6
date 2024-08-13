5bf06f69b1f73608723708f1e9a17712088013d4
Rafael Sartori <160548884+RafaelLSartori@users.noreply.github.com>
2024-08-12 23:08:07 -0300

    Atualização

--- src/view/Main.java
+++ src/view/Main.java
@@ -3,17 +3,17 @@ import controller.Ex6;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Ex6 m = new Ex6();
-		int vet10[] = {-12, -10, -7, -5, -3, 0, 1, 3, 5, 6};
-		int vet11[] = {-12, -10, -7, -5, -3, 0, 1, 3, 5, 6, 8};
-		int tamanho = vet10.length;
-		int vet = m.invertevetor(vet10, 0, tamanho);
-		
-		for(int i = 0; i < tamanho; i++) {
-			System.out.print(resp[i]);
-		}
-		
-		
-	}	
+    public static void main(String[] args) {
+        Ex6 m = new Ex6();
+        int vet10[] = {-12, -10, -7, -5, -3, 0, 1, 3, 5, 6};
+        int vet11[] = {-12, -10, -7, -5, -3, 0, 1, 3, 5, 6, 8};
+        int tamanho = vet10.length - 1;
+        int[] vet = m.invertevetor(vet10, 0, tamanho);
+
+        for(int i = 0; i < tamanho + 1; i++) {
+            System.out.print(vet[i] + ", ");
+        }
+
+
+    }
 }
